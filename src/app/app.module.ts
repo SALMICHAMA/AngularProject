3950b881527612a6cd78c00ebf424dfe509480e3
BURTE Victor <victor.burte@gmail.com>
2019-11-27 15:40:22 +0100

    create header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SingleOrganComponent } from './single-organ/single-organ.component';
 import {HttpClientModule} from '@angular/common/http';
 import { UpdateanimalComponent } from './updateanimal/updateanimal.component';
 import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 
 
 
@@ -38,7 +39,8 @@ const appRoutes: Routes = [
     AddanimalComponent,
     SingleAnimalComponent,
     SingleOrganComponent,
-    UpdateanimalComponent
+    UpdateanimalComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
